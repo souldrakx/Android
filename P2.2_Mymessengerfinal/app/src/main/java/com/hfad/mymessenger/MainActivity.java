222cee119530a6fd9556c332c99a8988015c7323
Christian <cjusto@uabc.edu.mx>
2022-02-16 17:07:38 -0800

    Practica 2.2 Messenger
    Parte final de la creación de app para enviar mensajes

---
+++ P2.2_Mymessengerfinal/app/src/main/java/com/hfad/mymessenger/MainActivity.java
@@ -0,0 +1,31 @@
+package com.hfad.mymessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSendMessage(View view){
+        EditText messageView = (EditText)findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plains");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+
+        String chooserTitle = getString(R.string.chooser);
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+
+        startActivity(chosenIntent);
+    }
+}
